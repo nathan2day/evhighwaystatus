b0759d982ddd671edc9db0f9f5bd92230ff6f6e1
Andrew Lees <mralees@me.com>
2016-12-15 13:58:08 +0000

    Move status tweets over to Laravel [WIP]

--- config/database.php
+++ config/database.php
@@ -65,6 +65,21 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+	
+	'evhws' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_EVHWS', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
