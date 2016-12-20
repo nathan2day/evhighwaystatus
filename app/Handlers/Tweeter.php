aa3211f787e83e9440dc7aec409a12ffe9e7b542
Andrew Lees <mralees@me.com>
2016-12-20 12:32:27 +0000

    Add Slack notifications

--- app/Handlers/Tweeter.php
+++ app/Handlers/Tweeter.php
@@ -3,16 +3,31 @@
 use Abraham\TwitterOAuth\TwitterOAuth;
 use Illuminate\Support\Facades\DB;
 use App\Repositories\ChargerRepository;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\StatusTweetSent;
+use App\Notifications\StatusTweetError;
 
 class Tweeter
 {
+       use Notifiable;
+
 	protected $chargers;
+
+	public function routeNotificationForSlack()
+        {
+	    return env('ADMIN_SLACK_URL');
+        }
 
 	public function __construct()
 	{
 		$this->tweeter = app('TweetSender');
 	}
 
+	public function testNotification()
+	{
+		$this->notify(new StatusTweetSent);
+		$this->notify(new StatusTweetError);
+	}
 	public function post($status)
 	{
 		$this->tweeter->post("statuses/update",[
@@ -20,8 +35,10 @@ class Tweeter
 		]);
 
 		if ($this->tweeter->getLastHttpCode() == 200) {
+			$this->notify(new StatusTweetSent);
     			return true;
 		} else {
+			$this->notify(new StatusTweetError);
    			return false;
 		}
 	}
